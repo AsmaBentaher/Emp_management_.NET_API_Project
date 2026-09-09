0223204e6a139b284c8f47b2bd65b60ec3d832b4
AsmaBentaher <asmaa.bentaherr@gmail.com>
2026-09-09 14:33:10 +0200

    Setup Api Controller & Dependency Injection & Connection String & Migration

--- EmployeeManagement.Api/Program.cs
+++ EmployeeManagement.Api/Program.cs
@@ -1,8 +1,22 @@
+using EmployeeManagement.Application.Interfaces;
+using EmployeeManagement.Application.Services;
+using EmployeeManagement.Infrastructure.Data;
+using EmployeeManagement.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("EmployeeManagement.Api")));
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<EmployeeService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
