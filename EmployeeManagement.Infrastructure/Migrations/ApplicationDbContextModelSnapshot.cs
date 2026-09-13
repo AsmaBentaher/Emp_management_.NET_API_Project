a1acbe49f163cb4f0a53521de54e0cc0e5ae6a88
AsmaBentaher <asmaa.bentaherr@gmail.com>
2026-09-13 10:44:55 +0200

    Change Migration to Infrastructure

--- EmployeeManagement.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ EmployeeManagement.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,19 +4,16 @@ using EmployeeManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace EmployeeManagement.Api.Migrations
+namespace EmployeeManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260909123059_InitialCreate")]
-    partial class InitialCreate
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
